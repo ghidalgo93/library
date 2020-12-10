8b56077b4f1e3d40fc4c8f8bee0ef0fd1931c091
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2020-12-10 09:17:58 -0700

    add project config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,32 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	mode: 'development', 
 	entry: './src/index.js',
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: 'Development',
-		})
-	], 
-	output: {
-		    filename: 'main.js',
-		    path: path.resolve(__dirname, 'dist'),
-	},
 	devtool: 'source-map', 
 	devServer: {
 		contentBase: './dist', 
 	}, 
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			}, 
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			}
+		]
+	}
 };
