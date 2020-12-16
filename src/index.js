3bdeb6ef7c63c74ec1ba1d40ee556eb81fa6b391
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2020-12-16 12:20:42 -0700

    cleaned up index.js

--- src/index.js
+++ src/index.js
@@ -9,25 +9,17 @@ import { displayLibrary } from "./displayControl";
 
 let myLibrary = [];
 
-// library container
 const libraryContainer = document.getElementById("library-container");
-
-// form
-const newBookForm = document.getElementById("new-form");
-
-// modal (ie popup)
 const newBookModal = document.getElementById("new-modal");
+const closeBtn = document.getElementsByClassName("close")[0];
+const newBookForm = document.getElementById("new-form");
 const newBookBtn = document.getElementById("new-btn");
-const span = document.getElementsByClassName("close")[0];
-
-// add-book
 const addBtn = document.getElementById("add-btn");
+
 addBtn.onclick = (e) => {
   e.preventDefault();
   const inputs = e.target.form.elements;
-
   if (verifyInputs(inputs)) {
-    newBookModal.style.display = "none";
     const newBook = Book(
       inputs.title.value,
       inputs.author.value,
@@ -35,12 +27,12 @@ addBtn.onclick = (e) => {
       inputs.read.checked
     );
     myLibrary = addBookToLibrary(newBook, myLibrary);
+    newBookModal.style.display = "none";
     newBookForm.reset();
     displayLibrary(myLibrary, libraryContainer);
   }
   // else, give 'required' styles of some kind
 };
-
 libraryContainer.addEventListener("click", function (e) {
   if (e.target && e.target.className === "remove-btn") {
     myLibrary = removeBookFromLibrary(e.target.dataset.index, myLibrary);
@@ -50,14 +42,10 @@ libraryContainer.addEventListener("click", function (e) {
     displayLibrary(myLibrary, libraryContainer);
   }
 });
-
-// When the user clicks on the button, open the modal
 newBookBtn.onclick = () => {
   newBookModal.style.display = "block";
 };
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = () => {
+closeBtn.onclick = () => {
   newBookModal.style.display = "none";
   newBookForm.reset();
 };
